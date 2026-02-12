ab6a732d97b7ecfa0a663096ee60d8e7d8f194e9
Waldo Oosthuizen <waldooosthuizen1234@gmail.com>
2026-02-12 12:55:49 +0200

    Fixed button color

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -176,7 +176,7 @@ const SignUp = ({ setShowSignUp }) => {
           <button
             type="submit"
             disabled={isLoading} // Disable the button while loading
-            className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 disabled:opacity-50 disabled:cursor-not-allowed">
+            className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-500 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 disabled:opacity-50 disabled:cursor-not-allowed">
             {isLoading ? (
               <Loader className="animate-spin mx-auto" /> // Show loading spinner if isLoading is true
             ) : (
